c0f3097c04a900b50c17a09f50b7e61aa3225987
Auryn Engel <auryn.engel@sap.com>
2022-05-23 12:36:00 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Bionic Reading implementation in Elm
+
+- [Owner](https://bionic-reading.com/)
+
+Since the German Patent is rejected and the there is just a trademark in us, this should be fine.
+
+## Getting started
+
+```sh
+yarn
+# start live server
+yarn live
+
+# build for dev
+yarn build
+
+# build for prod
+yarn optimize
+# replace in index.html the js to min.js
+```
+
+## Links
+
+- [Patent](https://depatisnet.dpma.de/DepatisNet/depatisnet?window=1&space=main&content=familie&action=treffer&docid=DE102017112916A1&firstdoc=1)
+- [German Patent](https://depatisnet.dpma.de/DepatisNet/depatisnet?action=pdf&docid=DE102017112916A1&xxxfull=1)
+- [US Patent](https://worldwide.espacenet.com/maximizedOriginalDocument?ND=4&flavour=maximizedPlainPage&locale=en_EP&FT=D&date=20171214&CC=US&NR=2017358238A1&KC=A1)
+- [CSS Theme](https://milligram.io/)
---
+++ src/Main.elm
@@ -0,0 +1,136 @@
+module Main exposing (..)
+
+import Browser
+import Html exposing (Html, b, div, h1, h3, span, text, textarea)
+import Html.Attributes exposing (placeholder, style)
+import Html.Events exposing (onInput)
+
+
+
+-- MAIN
+
+
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { content : String
+    }
+
+
+
+-- "Bionic Reading is a new method facilitating the reading process by guiding the eyes through text points. As a result, the reader is only focusing on the highlighted initial letters and letz the brain center complete the word. In a digital world dominated by shallow forms of reading, Bionic Reading aims to encourage a more in-depth reading and understanding of written content."
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Model "Start typing..."
+    , Cmd.none
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = UpdateContent String
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        UpdateContent content ->
+            ( { model | content = content }
+            , Cmd.none
+            )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub msg
+subscriptions _ =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div
+        [ style "padding" "2rem" ]
+        [ h1 [] [ text "Bionic Reading Elm" ]
+        , div [ style "display" "flex", style "flex-direction" "row", style "gap" "1rem" ]
+            [ div [ style "flex" "1" ] [ h3 [] [ text "Insert Text:" ], textarea [ style "min-height" "30vh", onInput UpdateContent, placeholder model.content ] [] ]
+            , div [ style "flex" "1" ] [ h3 [] [ text "Read Text:" ], contentView model.content ]
+            ]
+        ]
+
+
+contentView : String -> Html msg
+contentView content =
+    String.split " " content |> List.concatMap formatWordAndAddWhiteSpace |> div []
+
+
+formatWordAndAddWhiteSpace : String -> List (Html msg)
+formatWordAndAddWhiteSpace word =
+    let
+        formatedWord =
+            if wordStartWithNumber word then
+                [ text word ]
+
+            else
+                formatWord word
+    in
+    List.append [ whiteSpace ] formatedWord
+
+
+formatWord : String -> List (Html msg)
+formatWord word =
+    let
+        wordLength =
+            String.filter (\c -> c /= '.') word |> String.length
+    in
+    case wordLength of
+        1 ->
+            [ b [] [ text word ] ]
+
+        2 ->
+            [ String.left 1 word |> text |> List.singleton |> b [], String.right 1 word |> text ]
+
+        3 ->
+            [ String.left 1 word |> text |> List.singleton |> b [], String.right 2 word |> text ]
+
+        4 ->
+            [ String.left 2 word |> text |> List.singleton |> b [], String.right 2 word |> text ]
+
+        _ ->
+            let
+                midle =
+                    floor (toFloat wordLength * 3 / 5)
+            in
+            [ String.left midle word |> text |> List.singleton |> b [], String.right (String.length word - midle) word |> text ]
+
+
+wordStartWithNumber : String -> Bool
+wordStartWithNumber word =
+    List.range 0 9 |> List.map String.fromInt |> List.any (\c -> String.startsWith c word)
+
+
+whiteSpace : Html msg
+whiteSpace =
+    text " "
